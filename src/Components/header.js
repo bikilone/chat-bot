dfe5762db75aeb4fe3929d07826ba1e509cdfe3e
bikilone <milosbirovljevic@gmail.com>
2018-05-29 08:51:16 +0200

    header styling

---
+++ src/Components/header.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+import "./css/header.css"
+
+const Header = (props) => {
+
+    return (
+        <div className="header">
+            <img src="https://i.pinimg.com/originals/eb/01/7d/eb017dc0def701029b0efff741b01776.jpg" alt="" />
+            <p>Random name</p>
+        </div>
+    );
+}
+
+
+export default Header;
