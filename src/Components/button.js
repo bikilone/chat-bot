43290067f63305437d038205d52da83a3e44d012
bikilone <milosbirovljevic@gmail.com>
2018-05-29 21:21:46 +0200

    style

--- src/Components/button.js
+++ src/Components/button.js
@@ -9,7 +9,7 @@ const Button = (props) => {
         <div className="button">
             <img src={menu} alt="menu"/>
             <input onChange={props.handleChange} type="text" value={props.input} onKeyUp={props.handleSubmit}/>
-            <button onClick={props.handleClick}>Send |></button>
+            <button onClick={props.handleClick}>Send <icon className="fa fa-angle-right"></icon></button>
         </div>
     )
 }
