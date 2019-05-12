f2739386d3258dec26bb0f5f9f158a3c39eaad0d
bikilone <milosbirovljevic@gmail.com>
2019-05-12 11:36:12 +0200

    deployed

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 var express = require("express");
 var socket = require("socket.io");
+const path = require("path");
 
 var app = express();
 var server = app.listen(4000, function() {
   console.log("listening to request on port 4000");
 });
+
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static("build"));
+
+  app.get("*", (req, res) => {
+    res.sendfile(path.resolve(__dirname, "build", "index.html"));
+  });
+}
 
 // socket setup
 var io = socket(server);
